94a7d745fb2c06bf2ec1de3cee377ae6af0bc38d
Frank Guoi <frank.guoi@hotmail.fr>
2020-05-26 16:28:19 +0200

    Enable CORS

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // enable CORS
 app.use(function(req, res, next) {
- 	res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+ 	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
  	next();
 });
