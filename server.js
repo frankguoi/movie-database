671aab0a90255ace26bbdce1b6cdfa0de9ce1382
Frank Guoi <frank.guoi@hotmail.fr>
2020-05-26 16:27:52 +0200

    Enable CORS

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(bodyParser.json());
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// enable CORS
+app.use(function(req, res, next) {
+ 	res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+ 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ 	next();
+});
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Movie Database application." });
